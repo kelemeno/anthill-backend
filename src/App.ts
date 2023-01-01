dbd985dc71debce8d9f6eb4b698f865dce8a6146
Kalman Lajko <lajkokala@yahoo.com>
2023-01-01 21:58:26 +0100

    we send single nodes now

--- src/App.ts
+++ src/App.ts
@@ -13,12 +13,17 @@ app.use(function(req, res, next) {
 
 app.get("/root", function(req, res) {
    
-    res.send(getNeighbourhood(anthillRootId()));
+    res.send(anthillGraph[anthillRootId()]);
 });    
 
-app.get("/:id", function(req, res) {
+app.get("/relRootDepth", function(req, res) {
+   
+    res.send(relRootDepth);
+});
+
+app.get("/id/:id", function(req, res) {
  
-    res.send(getNeighbourhood(req.params.id));
+    res.send(anthillGraph[req.params.id]);
 
 });
 
@@ -34,34 +39,79 @@ app.listen(port, function() {
     crawlEthereum();
     console.log("Finished crawling ethereum for data");
 
-    readRelRootDepth();
-
+    getAnthillRelRootDepth();
 });
 
 
+//////////////////////
 
+// web3
 var Web3 = require('web3');
-
-const antHillContractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const anvilUrl = "http://localhost:8545";
 var web3 = new Web3(anvilUrl);
 
+// contract
+const antHillContractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+var fs = require('fs');
+var jsonFile = "../anthill/out/Anthill.sol/Anthill.json"
+var contract= JSON.parse(fs.readFileSync(jsonFile));
+var AnthillContract = new web3.eth.Contract(contract.abi, antHillContractAddress);
 
 
+// types
 type GraphData= {[id: string]: NodeData;}
-type NodeData = {"id":string, "parentIds":string[], childIds:string[], loaded: boolean}
+type NodeData = {"id":string, "name":string, "parentIds":string[], "treeParentId": string, "childIds":string[], }
 
 var anthillGraph = {} as GraphData;
 var relRootDepth = 6;
 
-var fs = require('fs');
-var jsonFile = "../anthill/out/Anthill.sol/Anthill.json"
-var contract= JSON.parse(fs.readFileSync(jsonFile));
-
-var AnthillContract = new web3.eth.Contract(contract.abi, antHillContractAddress);
 
 function anthillRootId() {
+    // we will need to add a method in the smart contract to get the root id
     return "0x0000000000000000000000000000000000000002";
+}
+
+
+async function getAnthillRelRootDepth() {
+    const res = await AnthillContract.methods.readRelRootDepth().call();
+    relRootDepth = res;
+    
+}
+
+async function loadDagVotes(id : string) : Promise<string[]>{
+    var dagVotes= [];
+    
+    var sentDagVoteDiffString = await AnthillContract.methods.readSentDagVoteDiff(id).call();
+    var sentDagVoteDiff = parseInt(sentDagVoteDiffString);
+    var sentDagVoteCountString="" ;
+
+
+    for (var i = 0; i < relRootDepth; i++) {
+        sentDagVoteCountString  = await AnthillContract.methods.readSentDagVoteCount(id, i+sentDagVoteDiff).call();
+        for (var j= 0; j< parseInt(sentDagVoteCountString); j++){
+            var childId = await AnthillContract.methods.readSentDagVote(id, i+sentDagVoteDiff, j).call();
+            dagVotes.push(childId);
+        }
+    }
+    return dagVotes;
+}
+
+async function loadSaveChildren(id: string){
+    // we set this false, as this is used on the client's side
+
+    var childCount  = await AnthillContract.methods.readRecTreeVoteCount(id).call();
+    for (var i = 0; i < childCount; i++) {
+        var childId = await AnthillContract.methods.readRecTreeVote(id, i).call();
+        var childName = childId.slice(-4)
+
+        var dagVotes: string[]= await loadDagVotes(childId);
+
+        var childNode = {"id": childId, "name":childName, "parentIds": dagVotes, "treeParentId": id, "childIds": []} as NodeData;
+        anthillGraph[id].childIds.push(childId);
+        anthillGraph[childId] = childNode;
+        loadSaveChildren(childId);
+    }
+    
 }
 
 async function crawlEthereum() {
@@ -71,189 +121,10 @@ async function crawlEthereum() {
 
     //call root, we fixed it as 2:
     var rootId : string= anthillRootId();
-    var node = {"id": rootId, "parentIds": [], "childIds": [], "loaded": false} as NodeData;
+    var rootName : string= rootId.slice(-4);
+
+    var node = {"id": rootId, "name": rootName, "parentIds": [], "treeParentId": "" ,  "childIds": []} as NodeData;
     anthillGraph[rootId] = node;
 
     loadSaveChildren(rootId);
 }
-
-async function readRelRootDepth() {
-    // what we want to do here is call the root method in the smart contract, this should return an address
-    // with this address we can start crawling the tree
-    // for each node we visit, we want to add it to the AnthillGraph
-
-    //call root, we fixed it as 2:
-    const res = await AnthillContract.methods.readRelRootDepth().call();
-    relRootDepth = res;
-}
-
-async function loadSaveChildren(id: string){
-    anthillGraph[id].loaded = true;
-
-    var childCount  = await AnthillContract.methods.readRecTreeVoteCount(id).call();
-    for (var i = 0; i < childCount; i++) {
-        var childId = await AnthillContract.methods.readRecTreeVote(id, i).call();
-        var childNode = {"id": childId, "parentIds": [id], "childIds": [], "loaded": false} as NodeData;
-        anthillGraph[childId] = childNode;
-        anthillGraph[id].childIds.push(childId);
-        loadSaveChildren(childId);
-    }
-}
-
-
-function getNeighbourhood(id : string) : GraphData{
-    var neighbourhood = {} as GraphData;
-    var node = anthillGraph[id];
-    neighbourhood[id] = node;
-    var parent = node;
-
-    for (var i = 0; i < relRootDepth; i++) {
-        var parentId = parent.parentIds[i];
-        neighbourhood[parentId] = anthillGraph[parentId];
-    }
-    
-    for (var i = 0; i < node.childIds.length; i++) {
-        var childId = node.childIds[i];
-        neighbourhood[childId] = anthillGraph[childId];
-    }
-    return neighbourhood;
-}
-
-
-// const OrgChart={
-//   "Root": {
-//     "id": "Root",
-//     "parentIds" : [],
-//     "childIds": ["Cain", "Seth", "Abel", "Awan", "Enoch", "Azura"]
-//     , "loaded": true, 
-
-//   },
-//   "Cain":{
-//     "id": "Cain",
-//     "parentIds": ["Root"],
-//     "childIds": []
-//     , "loaded": true, 
-
-//   },
-//   "Seth":{
-//     "id": "Seth",
-//     "parentIds": ["Root"],
-//     "childIds": ["Enos", "Noam"]
-//     , "loaded": true, 
-
-//   },
-//   "Enos":{
-//     "id": "Enos",
-//     "parentIds": ["Seth"],
-//     "childIds": ["C6"]
-//     , "loaded": true, 
-
-//   },
-//   "Noam":{
-//     "id": "Noam",
-//     "parentIds": ["Seth"],
-//     "childIds": ["C3", "C4"]
-//     , "loaded": true, 
-
-//   },
-//   "Abel":{
-//     "id": "Abel",
-//     "parentIds": ["Root"],
-//     "childIds": ["C3", "C2", "C1"]
-//     , "loaded": true, 
-
-//   },
-//   "Awan":{
-//     "id": "Awan",
-//     "parentIds": ["Root", "Abel"],
-//     "childIds": ["C5", "C7", "C8", "C9", ]
-//     , "loaded": true, 
-
-//   },
-//   "Enoch":{
-//     "id": "Enoch",
-//     "parentIds": ["Root"],
-//     "childIds": []
-//     , "loaded": true, 
-
-//   },
-//   "Azura":{
-//     "id": "Azura",
-//     "parentIds": ["Root"],
-//     "childIds": []
-//     , "loaded": true, 
-
-//   },
-//   "C1":{
-//     "id": "C1",
-//     "parentIds": ["Abel"],
-//     "childIds": ["C5"]
-//     , "loaded": true, 
-
-//   },
-//   "C2":{
-//     "id": "C2",
-//     "parentIds": ["Abel"],
-//     "childIds": ["C4", "C5"]
-//     , "loaded": true, 
-
-//   },
-//   "C3":{
-//     "id": "C3",
-//     "parentIds": ["Abel", "Noam"],
-//     "childIds": ["C4", "C7"]
-//     , "loaded": true, 
-
-//   },
-//   "C4":{
-//     "id": "C4",
-//     "parentIds": ["Noam", "C3", "C2"],
-//     "childIds": ["C8", "C9"]
-//     , "loaded": true, 
-
-
-//   },
-//   "C5":{
-//     "id": "C5",
-//     "parentIds": ["Awan", "C2", "C1"],
-//     "childIds": ["C8", "C9", "C10"]
-//     , "loaded": true, 
-
-//   },
-//   "C6":{
-//     "id": "C6",
-//     "parentIds": ["Enos", "Seth"],
-//     "childIds": ["C7"]
-//     , "loaded": true, 
-
-//   },
-//   "C7":{
-//     "id": "C7",
-//     "parentIds": ["Awan", "C3", "C6"],
-//     "childIds": []
-//     , "loaded": true, 
-
-//   },
-//   "C8":{
-//     "id": "C8",
-//     "parentIds": ["Awan", "C4", "C5"],
-//     "childIds": ["C10"]
-//     , "loaded": true, 
-
-//   },
-//   "C9":{
-//     "id": "C9",
-//     "parentIds": ["Awan", "C4", "C5"],
-//     "childIds": ["C10"]
-//     , "loaded": true, 
-
-//   },
-//   "C10":{
-//     "id": "C10",
-//     "parentIds": ["C8", "C9", "C5"],
-//     "childIds": []
-//     , "loaded": true, 
-
-//   },
-// } as GraphData
-// export default OrgChart 
